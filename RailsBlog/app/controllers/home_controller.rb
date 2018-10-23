99d5ee746572a33c8b458940e3d0e7c7232599d2
D4v1d98Ru1z <davidl.ruizc@gmail.com>
2018-10-23 17:44:42 -0500

    UPDATE: having the data from the inputs

--- RailsBlog/app/controllers/home_controller.rb
+++ RailsBlog/app/controllers/home_controller.rb
@@ -1,7 +1,22 @@
 class HomeController < ApplicationController
   def index
+    @nposts = Home.all
   end
   def new
     @npost = Home.new    
+  end
+  def create 
+    @npost = Home.new(post_params)
+    if @npost.save
+      redirect_to  home_path, notice: "Post created"
+    else 
+      render :new, alert: "Post wasn't created"
+    end
+  end
+  
+  #define a method for post params private
+  private
+  def post_params
+    params.require(:post).permit(:title, :description)
   end
 end
--- RailsBlog/config/routes.rb
+++ RailsBlog/config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   root 'home#index'
   get '/new', to: "home#new"
   get '/blog', to: "blog#index"
+  #where I'm going to redirect the data
+  get '/', to: "home#index"
   resources :home
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
